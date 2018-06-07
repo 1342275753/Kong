7f6e8bff01d4a3f58bd65e19cfcc30482d7a3e9f
1342275753 <www.1342275753@qq.com>
2018-06-07 15:49:09 +0800

    modify

---
+++ js/Qpeople.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2018/6/7.
+ */
+function qipeopen() {
+    alert("里面没东西");
+    alert("都说了木有");
+    alert("该放弃了");
+    alert("是时候停止了");
+    alert("咋就是不信呢")
+}
+alert("猜猜里面有啥");
+qipeopen();
+alert("别坚持了");
+qipeopen();
+alert("到底了！88");
